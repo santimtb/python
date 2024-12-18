ec1232cc73553705df0fc441873dd5d1065c7348
smedina <smedina@mjosefacampos.com>
2024-12-18 22:51:47 +0100

    Crear .env e instalar pip install python-dotenv

--- divisas/cambio.py
+++ divisas/cambio.py
@@ -1,8 +1,14 @@
 import requests
+from dotenv import load_dotenv
+import os
 
-api_key = 'abe92a0da5161d1cf50b9447'
+# Carga las variables de entorno del archivo .env
+load_dotenv()
+
+api_key = os.getenv('API_KEY')
+
 # Where USD is the base currency you want to use
-url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/USD'
+url = f'https://v6.exchangerate-api.com/v6/{api_key}/latest/EUR'
 print(url)
 # Making our request
 response = requests.get(url)
