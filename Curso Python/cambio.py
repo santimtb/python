3db033d516f975d60c9b3e9013c3cd520ed11042
smedina <smedina@mjosefacampos.com>
2025-01-06 20:24:09 +0100

    Creación de area.py y tabla.py

--- Curso Python/cambio.py
+++ Curso Python/cambio.py
@@ -1,13 +1,23 @@
-valores = (500,200,100,50,20,10,5,2,1,0.5,0.2,0.1,0.05,0.02,0.01)
+billetes = (500,200,100,50,20,10,5)
+monedas = (2,1,0.5,0.2,0.1,0.05,0.02,0.01)
 
 while True:
     try:
         precio = float(input("Introduce la cantidad a cambiar: "))
+        if precio<0:
+            raise Exception("El valor introducido debe ser positivo")
         break
     except ValueError:
         print("Debes introducir un número válido")
+    except Exception as e:
+        print(e)
 
-for valor in valores:
+for valor in billetes:
     if precio // valor:
         print(f"Billetes de {valor}: {int(precio // valor)}")
+        precio=round(precio % valor,2)
+
+for valor in monedas:
+    if precio // valor:
+        print(f"Monedas de {valor}: {int(precio // valor)}")
         precio=round(precio % valor,2)
